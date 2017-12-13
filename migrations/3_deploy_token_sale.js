581c411b86335b77a930743194a3c1387f09ce39
Sim <sim@sliver.tv>
2017-12-12 18:19:39 -0800

    rename adminAddr in deploy script to deployerAddr

--- migrations/3_deploy_token_sale.js
+++ migrations/3_deploy_token_sale.js
@@ -2,17 +2,18 @@ var ThetaTokenSale = artifacts.require('ThetaTokenSale');
 
 module.exports = function (deployer, network, accounts) {
     var rootAddr     = '';
-    var adminAddr    = '';
+    var deployerAddr = '';
     var whiteListControllerAddr    = '';
     var exchangeRateControllerAddr = '';
     var thetaLabReserveAddr = '';
     var fundDepositAddr     = '';
+
     var initialBlock = 223583611111;
     var finalBlock   = 223583699999;
     var exchangeRate = 30000;
 
     console.log('---------3----------');
-    deployer.deploy(ThetaTokenSale, rootAddr, adminAddr, 
+    deployer.deploy(ThetaTokenSale, rootAddr, deployerAddr, 
         whiteListControllerAddr, exchangeRateControllerAddr, thetaLabReserveAddr, fundDepositAddr,
         initialBlock, finalBlock, exchangeRate);
 };
