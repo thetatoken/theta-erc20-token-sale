be43aec0c1db6c11782d919afe9a28cc04c333fc
Jieyi Long <jieyi@Sheetals-MacBook-Pro.local>
2018-01-05 17:08:55 -0800

    Fixes for the TimelockedSafe

--- contracts/TimelockedSafe.sol
+++ contracts/TimelockedSafe.sol
@@ -11,52 +11,59 @@ import "./ThetaToken.sol";
 
 contract TimelockedSafe {
 
+    using SafeMath for uint;
+
 	uint constant public decimals = 18;
 
-    using SafeMath for uint;
+	uint constant public oneMonth = 30 days;
 
     address public withdrawAddress;
 
-    uint public maximumBalanceInWei;
+    uint public lockingPeriodInMonths; // all tokens are locked for a certain period
 
-    uint public cliffInMonths; // locking period in terms of months
+    uint public vestingPeriodInMonths; // after the locking period ends the vesting period starts, but each month
+                                       // there is a token withdraw limit as defined below
 
-    uint public monthlyWithdrawLimitInWei; // monthly withdraw limit AFTER the cliff
+    uint public monthlyWithdrawLimitInWei; // monthly withdraw limit during the vesting period
 
     ThetaToken public token;
 
     uint public startTime;
 
-    function TimelockedSafe(address _withdrawAddress, 
-    	uint _maximumBalanceInWei, uint _cliffInMonths, 
+    function TimelockedSafe(address _withdrawAddress,
+    	uint _lockingPeriodInMonths, uint _vestingPeriodInMonths,
     	uint _monthlyWithdrawLimitInWei, address _token) public {
+    	require(_withdrawAddress != 0);
+    	require(_token != 0);
 
-    	// just to prevent mistakenly using an incorrect token unit
-    	require(_maximumBalanceInWei > 10000 * (10 ** decimals));
+    	// just to prevent mistakenly passing in a value with incorrect token unit
     	require(_monthlyWithdrawLimitInWei > 100 * (10 ** decimals));
 
     	withdrawAddress = _withdrawAddress;
-    	maximumBalanceInWei = _maximumBalanceInWei;
-    	cliffInMonths = _cliffInMonths;
+    	lockingPeriodInMonths = _lockingPeriodInMonths;
+    	vestingPeriodInMonths = _vestingPeriodInMonths;
     	monthlyWithdrawLimitInWei = _monthlyWithdrawLimitInWei;
-    	token = ThetaToken(address);
+    	token = ThetaToken(_token);
     	startTime = now;
     }
 
-    function withdraw(uint amountInWei) returns (bool) public {
-    	uint oneMonth = 30 days;
+    function withdraw(uint _withdrawAmountInWei) returns (bool) public {    	
     	uint timeElapsed = now.sub(startTime);
-    	uint monthsElapsed = timeElapsed.div(oneMonth);
-    	uint vestingMonth = monthsElapsed.sub(cliffInMonths).add(1);
-    	require(vestingMonth >= 0);
+    	uint monthsElapsed = (timeElapsed.div(oneMonth)).add(1);
+    	require(monthsElapsed >= lockingPeriodInMonths);
+
+    	uint fullyVestedTimeInMonths = lockingPeriodInMonths.add(vestingPeriodInMonths);
+    	uint remainingVestingPeriodInMonths = 0;
+    	if (monthsElapsed < fullyVestedTimeInMonths) {
+    		remainingVestingPeriodInMonths = fullyVestedTimeInMonths.sub(monthsElapsed);
+    	}
 
     	address timelockedSafeAddress = address(this);
-    	uint withdrawLimit = monthlyWithdrawLimitInWei.mul(vestingMonth);
-    	uint minimumBalanceInWei = maximumBalanceInWei.sub(withdrawLimit);
+    	uint minimalBalanceInWei = remainingVestingPeriodInMonths.mul(monthlyWithdrawLimitInWei);
     	uint currentTokenBalanceInWei = token.balanceOf(timelockedSafeAddress);
-    	require(currentTokenBalanceInWei.sub(amountInWei) >= minimumBalanceInWei);
+    	require(currentTokenBalanceInWei.sub(_withdrawAmountInWei) >= minimalBalanceInWei);
 
-    	require(token.transfer(withdrawAddress, amountInWei));
+    	require(token.transfer(withdrawAddress, _withdrawAmountInWei));
 
     	return true;
     }
